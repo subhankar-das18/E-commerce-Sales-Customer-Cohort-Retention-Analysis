03ea5e39c6100c161169ee5c74d45b3320837d0c
Subhankar Das <subhankardas.work@outlook.com>
2026-09-08 21:22:59 +0530

    Update JOIN to use olist_customers_dataset

--- sql/05_delivery_analysis.sql
+++ sql/05_delivery_analysis.sql
@@ -16,7 +16,7 @@ SELECT
     ROUND(100.0 * SUM(CASE WHEN o.order_delivered_customer_date > o.order_estimated_delivery_date THEN 1 ELSE 0 END)
         / COUNT(*), 2) AS pct_late
 FROM olist_orders_dataset o
-JOIN customers c ON o.customer_id = c.customer_id
+JOIN olist_customers_dataset c ON o.customer_id = c.customer_id
 WHERE o.order_status = 'delivered'
   AND o.order_delivered_customer_date IS NOT NULL
 GROUP BY c.customer_state
